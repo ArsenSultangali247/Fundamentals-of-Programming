f3a51b675291c822a46ce8c146dac157a95e1de2
ArsenSultangali247 <sultangali24@gmail.com>
2021-01-31 22:54:51 +0600

    c#

---
+++ lab3/lab3ex3/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace lab3ex3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Write a binary number");
+            var a = Console.ReadLine();
+
+            var hex = Convert.ToInt32(a, 2).ToString("X");
+
+            Console.WriteLine($"Hex is {hex}");
+        }
+    }
+}
